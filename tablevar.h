34201940ba66f731bf6170e2feabcb192fb9000d
Christopher Haster <chaster@utexas.edu>
2013-05-20 22:48:43 -0500

    Added table set and lookup functions

--- tablevar.h
+++ tablevar.h
@@ -15,4 +15,13 @@ typedef entry_t *table_t;
 #define var_table(v) (*(entry_t**)((v).data))
 
 
+// looks up a key in the table and 
+// returns either its value or null
+var_t table_lookup(var_t table, var_t key);
+
+
+// sets a value in the table with the given key
+void table_set(var_t table, var_t key, var_t val);
+
+
 #endif
